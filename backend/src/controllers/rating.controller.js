11aa2f31ea4ffc9b190397ea17f7eccd9449ea73
Hairii <dugarcein.leila16@gmail.com>
2026-03-12 16:42:39 +0100

    controller pour ajouter notes

--- backend/src/controllers/rating.controller.js
+++ backend/src/controllers/rating.controller.js
@@ -9,3 +9,15 @@ export const getAllRatings = async (req, res) => {
         res.status(500).json({message: 'erreur server (getAllRatings'})
     }
 };
+
+export const addRating = async (req, res) => {
+    try{
+        const {gameID, rating} = req.body;
+        const userID = req.user.id; // recuperer par le token
+        await createRating(gameID, userID, rating);
+        res.status(201).json ({message: "rating créer"});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message: 'erreur server (creatRating'});  
+    }
+}
