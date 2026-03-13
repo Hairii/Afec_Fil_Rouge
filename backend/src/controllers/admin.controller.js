95e29f7d2c75c9df0ed3bbaceda39c1e06dacbca
Hairii <61686604+Hairii@users.noreply.github.com>
2026-03-13 13:16:59 +0100

    back pour admin

---
+++ backend/src/controllers/admin.controller.js
@@ -0,0 +1,90 @@
+import {
+  deleteGame,
+  updateGame,
+  getReporteComments,
+  unreportComment,
+  deleteComment,
+  deleteUser,
+} from "../models/admin.model.js";
+
+export const deletedGame = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await deleteGame(id),
+      [game] = await db.query("SELECT * FROM games WHERE id = ?", [id]);
+    if (!deleted) {
+      return res.status(404).json({ message: "jeu non trouvé" });
+    }
+    res.status(200).json({ message: "jeu supprimé" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "erreur server (deleteGame" });
+  }
+};
+
+export const patchGame = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, released } = req.body;
+    const update = await updateGame(id, name, released);
+    if (!update) {
+      return res.status(404).json({ messsage: "jeu non trouvé" });
+    }
+    res.status(200).json({ message: "jeu mis à jour" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "erreur server (updateGame" });
+  }
+};
+
+export const getReportedComments = async (req, res) => {
+    try {
+        const comments = await getReporteComments();
+        res.json(comments);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "erreur server (getReportedComments" });
+    }
+};
+
+export const unReportedComment = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const unreported = await unreportComment(id);
+        if(!unreported){
+            return res.status(404).json({message: 'commentaire non rencontré'});
+        }
+        res.status(200).json({message: 'commentaire non reporté'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "erreur server (unreportComment" });
+    }
+};
+
+export const deletedComments = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deleted = await deleteComment(id);
+        if(!deleted){
+            return res.status(404).json({message: 'commentaire non rencontré'});
+        }
+        res.status(200).json ({message: 'commentaire supprimé'});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message: 'erreur server (deleteComments'});
+    }
+}
+
+export const deletedUser = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deleted = await deleteUser(id);
+        if(!deleted){
+            return res.status(404).json({message: 'utilisateur non rencontré'});
+        }
+        res.status(200).json ({message: 'utilisateur supprimé'});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message: 'erreur server (deleteUser'});
+    }
+}   
---
+++ backend/src/models/admin.model.js
@@ -0,0 +1,57 @@
+import db from "../config/db.js";
+
+export const deleteGame = async (id) => {
+  try {
+    const [game] = await db.query("DELETE FROM games WHERE id = ?", [id]);
+    return game.affectedRows > 0;
+  } catch (error) {
+    console.error("erreur server (deleteGame)", error.message);
+  }
+};
+
+export const updateGame = async (id, name, released) => {
+  try {
+    const [game] = await db.query(
+      "UPDATE games SET name = ?, released = ? WHERE id = ?",
+      [name, released, id],
+    );
+  } catch (error) {
+    console.error("erreur server (updateGame)", error.message);
+    throw error;
+  }
+};
+
+export const getReporteComments = async () => {
+    try {
+        const [comments] = await db.query('SELECT * FROM comments WHERE reported = 1');
+        return comments;
+    } catch (error) {
+        console.error('erreur server (getReportedComments)', error.message);    
+    }
+};
+
+export const unreportComment = async (id) => {
+    try {
+        const [comment] = await db.query('UPDATE comments SET reported = 0 WHERE id = ?', [id]);
+    } catch (error) {
+        console.error('erreur server (unreportComment)', error.message);
+    }
+}
+
+export const deleteComment = async (id) => {
+    try {
+        const [comment] = await db.query ('DELETE FROM comments WHERE id = ?', [id]);
+    } catch (error) {
+        console.error('erreur server (deleteComment)', error.message);
+    }
+}
+
+export const deleteUser = async (id) => {
+  try {
+    await db.query('DELETE FROM comments WHERE user_id = ?', [id]);
+    await db.query('DELETE FROM ratings WHERE user_id = ?', [id]);
+    await db.query('DELETE FROM users WHERE id = ?', [id]);
+  } catch (error) {
+    console.error('erreur server (deleteUser)', error.message);
+  }
+};
