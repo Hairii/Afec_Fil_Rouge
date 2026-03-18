3428ad514feda515b176088fede5fd2cb2855ea3
Hairii <dugarcein.leila16@gmail.com>
2026-03-18 09:36:13 +0100

    les bonne method et fetch a faire

--- frontend/js/api/admin.api.js
+++ frontend/js/api/admin.api.js
@@ -1,92 +1,114 @@
-export const deleteGame = async (id) => {
-    try {
-        const response = await fetch ('http://localhost:3000/api/admin/delete', {
-            method: 'POST',
-            credentials: 'include', 
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ id }),
-        });
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-    }
-}
+const deleteGame = async (id) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/admin/games/${id}`,
+      {
+        method: "DELETE",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+      },
+    );
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export const updateGame = async (id, game, released) => {
-    try {
-        const response = await fetch ('http://localhost:3000/api/admin/update', {
-            method: ' PACTH',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ id, game, released }),
-        });
-        return await response.json();
-    } catch (error) {
-        console.error(error); 
-    }
-}
+const updateGame = async (id, name, released) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/admin/games/${id}`,
+      {
+        method: "PATCH",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, released }),
+      },
+    );
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export const getReportedComments = async () => {
-    try {
-        const response = await fetch ('http://localhost:3000/api/admin/report', {
-            credentials: 'include',
-        });
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-    }
-}
+const getReportedComments = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/admin/comments", {
+      credentials: "include",
+    });
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export const unreportComment = async (id) => {
-    try {
-        const response = await fetch ('http://localhost:3000/api/admin/unreport', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ id }),
-        });
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-    }
-}
+const unreportComment = async (id) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/admin/comments/${id}`,
+      {
+        method: "PATCH",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+      },
+    );
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export const deleteComment = async (id) => {
-    try {
-        const response = await fetch ('http://localhost:3000/api/admin/deleteComment', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ id }),
-        });
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-    }
-}
+const deleteComment = async (id) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/admin/comments/${id}`,
+      {
+        method: "DELETE",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+      },
+    );
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export const deleteUser = async (id) => {
-    try {
-        const response = await fetch ('http://localhost:3000/api/admin/deleteUser', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ id }),
-        });
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-    }
-}
+const deleteUser = async (id) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/admin/users/${id}`,
+      {
+        method: "DELETE",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+      },
+    );
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export {deleteGame, updateGame, getReportedComments, unreportComment, deleteComment, deleteUser};
+export {
+  deleteGame,
+  updateGame,
+  getReportedComments,
+  unreportComment,
+  deleteComment,
+  deleteUser,
+};
