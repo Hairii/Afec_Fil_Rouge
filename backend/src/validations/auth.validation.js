bac47f6915f436b9fe12d08282bb0effb9443f08
Hairii <dugarcein.leila16@gmail.com>
2026-06-10 14:34:36 +0200

    joi mieux sécurisé

--- backend/src/validations/auth.validation.js
+++ backend/src/validations/auth.validation.js
@@ -2,12 +2,43 @@ import Joi from 'joi';
 
 export const registerSchema = Joi.object({
     username: Joi.string().min(3).max(20).required(),
-    mail: Joi.string().email().required(),
-    password: Joi.string().min(8).required(), 
-   confirmPassword: Joi.string().valid(Joi.ref('password')).required(),// ref au password pour verif si c'est le même
-})
+
+     mail: Joi.string()
+        .email()
+        .trim()
+        .lowercase()
+        .required()
+        .messages({
+            'string.email': 'format de mail incorrect',
+        }),
+
+    password: Joi.string()
+        .min(8)
+        .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/)
+        .required()
+        .messages({
+            'string.min': 'Le mot de passe est trop court (minimum 8 caractères)',
+            'string.pattern.base': 'Le mot de passe doit contenir majuscule, minuscule et chiffre',
+            
+        }),
+
+    confirmPassword: Joi.string()
+        .valid(Joi.ref('password'))
+        .required()
+        .messages({
+            'any.only': 'Les mots de passe ne correspondent pas',
+        }),
+});
 
 export const loginSchema = Joi.object({
-    mail: Joi.string().email().required(),
-    password: Joi.string().required(), 
-})
+    mail: Joi.string()
+        .email()
+        .trim()
+        .lowercase()
+        .required()
+        .messages({
+            'string.email': 'format de mail incorrect',
+        }),
+
+    password: Joi.string().required(),
+});
